3b5ef07775c600c2bfbe71189c2d5fb6f78130cf
temujin05 <78592189+temujin05@users.noreply.github.com>
2025-07-29 12:54:36 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ $(window).bind('scroll', function (e) {
 
 function parallaxScroll() {
    var scrolled = $(window).scrollTop(); 
-   $('.item-1').css('top',(350-(scrolled *.5)) + 'px');
+   $('.item-1').css('top',(350-(scrolled*.5)) + 'px');
    $('.item-2').css('top',(180-(scrolled *.4)) + 'px');
    $('.item-3').css('top',(520-(scrolled *.3)) + 'px'); 
    $('.item-4').css('top',(580-(scrolled *.45)) + 'px'); 
